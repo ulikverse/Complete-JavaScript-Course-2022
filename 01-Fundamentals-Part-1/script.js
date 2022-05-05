480802ba9f11838a28de052cb65949885ad4ca37
Yulii Shtel <ulik1shtel@gmail.com>
2022-05-05 18:10:29 +0300

    Operator Precedence

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -59,7 +59,7 @@ job = "teacher";
 
 lastName = "Schmedtmann";
 console.log(lastName);
-*/
+
 
 // Math operators
 const now = 2037;
@@ -90,3 +90,17 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
