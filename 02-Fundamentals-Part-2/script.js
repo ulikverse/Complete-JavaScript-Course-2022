5907f8a89faaefee2e4acee5646e3b0a2f9686b3
Yulii Shtel <ulik1shtel@gmail.com>
2022-05-11 17:00:46 +0300

    Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,4 +1,5 @@
 "use strict";
+/*
 
 let hasDriversLicence = false;
 const passTest = true;
@@ -8,3 +9,27 @@ if (hasDriversLicence) console.log("I can drive :D");
 
 // const interface = "Audio";
 // const private = 534;
+
+*/
+
+function logger() {
+  console.log("My name is Ulik");
+}
+
+// calling / running / invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  // console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
