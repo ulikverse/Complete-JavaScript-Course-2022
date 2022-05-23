6727a122fcf99094e62a0d8d838162af507add20
Yulii Shtel <ulik1shtel@gmail.com>
2022-05-23 21:42:38 +0300

    Iteration: The for Loop

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -442,4 +442,20 @@ if (mark.bmi > john.bmi) {
     }
     
     //John's BMI (28.3) is higher than Mark's (23.9)!
+    
+    // console.log("Lifting weights repetition 1 🏋🏻‍♀️");
+    // console.log("Lifting weights repetition 2 🏋🏻‍♀️");
+    // console.log("Lifting weights repetition 3 🏋🏻‍♀️");
+    // console.log("Lifting weights repetition 4 🏋🏻‍♀️");
+    // console.log("Lifting weights repetition 5 🏋🏻‍♀️");
+    // console.log("Lifting weights repetition 6 🏋🏻‍♀️");
+    // console.log("Lifting weights repetition 7 🏋🏻‍♀️");
+    // console.log("Lifting weights repetition 8 🏋🏻‍♀️");
+    // console.log("Lifting weights repetition 9 🏋🏻‍♀️");
+    // console.log("Lifting weights repetition 10 🏋🏻‍♀️");
+    
+    // for loop keeps running while condition is TRUE
+    for (let rep = 1; rep <= 10; rep++) {
+      console.log(`Lifting weights repetition ${rep} 🏋🏻‍♀️`);
+    }
     */
