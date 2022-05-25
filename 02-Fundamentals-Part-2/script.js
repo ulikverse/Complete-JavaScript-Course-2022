2ca0b5959694e07d73c96f27870a4c79e6f836a4
Yulii Shtel <ulik1shtel@gmail.com>
2022-05-25 07:41:14 +0300

    Looping Backwards and Loops in Loops

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -511,4 +511,28 @@ if (mark.bmi > john.bmi) {
       console.log(jonas[i], typeof jonas[i]);
     }
     
+    
+    const jonas = [
+      "Jonas",
+      "Shmedtmann",
+      2037 - 1991,
+      "teacher",
+      ["Michael", "Peter", "Steven"],
+      true,
+    ];
+    
+    // 0, 1 ... 4
+    // 4, 3 ... 0
+    
+    for (let i = jonas.length - 1; i >= 0; i--) {
+      console.log(i, jonas[i]);
+    }
+    for (let exercise = 1; exercise < 4; exercise++) {
+      console.log(`----------- Starting exercise ${exercise}`);
+      
+      for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋🏻‍♀️`);
+      }
+    }
+    
     */
