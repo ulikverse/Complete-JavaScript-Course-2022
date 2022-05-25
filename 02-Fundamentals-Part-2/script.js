9e6c19583eb1fb95a80b3cd192cd775927d8d734
Yulii Shtel <ulik1shtel@gmail.com>
2022-05-25 08:24:48 +0300

    The while Loop

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -535,4 +535,22 @@ if (mark.bmi > john.bmi) {
       }
     }
     
-    */
+    
+    // for (let rep = 1; rep <= 10; rep++) {
+      //   console.log(`Lifting weights repetition ${rep} 🏋🏻‍♀️`);
+      // }
+      
+      let rep = 1;
+      while (rep <= 10) {
+        // console.log(`WHILE: Lifting weights repetition ${rep} 🏋🏻‍♀️`);
+        rep++;
+      }
+      
+      let dice = Math.trunc(Math.random() * 6) + 1;
+      
+      while (dice !== 6) {
+        console.log(`You rolled a ${dice}`);
+        dice = Math.trunc(Math.random() * 6) + 1;
+        if (dice === 6) console.log("Loop is about to end...");
+      }
+      */
