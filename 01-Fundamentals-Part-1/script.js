1ef489a41cfd052bebe628bb33b1b8728c42bb0a
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-02 19:53:32 +0300

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,7 +1,28 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let _function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
 
 /*
 let js = "amazing";
