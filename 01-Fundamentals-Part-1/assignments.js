e36f29b2fdbdc5f137c07f2dd7ec3479b4cb8b2f
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-04 19:26:35 +0300

    LECTURE: Data Types

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,9 +1,27 @@
-// LECTURE Values and Variables
+/*
+LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions) 
+2. Log their values to the console
+*/
 let country = "Ukraine";
 let continent = "Europe";
 let population = 42000000;
 
 console.log(country, continent, population);
+
+/*
+LECTURE: Data Types 
+1. Declare a variable called 'isIsland' and set its value according to your 
+country. The variable should hold a Boolean value. Also declare a variable 
+'language', but don't assign it any value yet 
+2. Log the types of 'isIsland', 'population', 'country' and 'language' 
+to the console
+*/
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland, population, country, language);
 
 /*
 
