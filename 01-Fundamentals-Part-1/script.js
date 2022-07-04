8e3d70fcb3c64a026f2680cbd3edb2c16e8f1281
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-04 19:42:03 +0300

    let, const and var

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -26,8 +26,6 @@ let job2 = "teacher";
 
 console.log(myFirstJob);
 
-*/
-
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -47,6 +45,20 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtmann";
+console.log(lastName);
 
 /*
 ----------------------------------------------------------
