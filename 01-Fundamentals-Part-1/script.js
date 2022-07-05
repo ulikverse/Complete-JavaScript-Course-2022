d9e208763da8a669d7c0466d47d2d330e9072bb1
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-05 19:51:30 +0300

    Strings and Template Literals

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -101,6 +101,28 @@ console.log(x, y);
 
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
+
+const firstName = "Jonas";
+const job = "Teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas =
+"I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log("String with \n\
+multiple \n\
+lines");
+
+console.log(`String
+multiple
+lines`);
 */
 
 /*
