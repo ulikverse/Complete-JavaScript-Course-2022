f67158ce6492ac258351213769c6447531a782fc
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-05 20:44:01 +0300

    Coding Challenge #2

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -126,7 +126,42 @@ if (population > 1300000000) {
     `${country}'s population is ${33000000 - population} million below average`
     );
   }
-  */
+*/
+
+/*
+Coding Challenge #2 
+
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it. 
+
+Your tasks: 
+1. Print a nice output to the console, saying who has the higher BMI. The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!" 
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!" 
+
+Hint: Use an if/else statement 😉 
+ 
+GOOD LUCK 😀
+*/
+
+const markMass = 78;
+const johnMass = 92;
+
+const johnHeight = 1.95;
+const markHeight = 1.69;
+
+// const markMass = 95;
+// const johnMass = 85;
+
+// const johnHeight = 1.76;
+// const markHeight = 1.88;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+} else {
+  console.log(`John's BMI (${johnBMI}) is higher than Mark's(${markBMI})!`);
+}
 
 /*
 -------------------------------------------------------------
