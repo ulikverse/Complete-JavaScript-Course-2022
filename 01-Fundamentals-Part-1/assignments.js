160b6687f81e930858f833c1835a1ff53bc5683a
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-05 20:30:57 +0300

    LECTURE: Taking Decisions: if / else Statements

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -107,11 +107,26 @@ console.log(markHigherBMI);
 /*
 LECTURE: Strings and Template Literals 
 1. Recreate the 'description' variable from the last assignment, this time using the template literal syntax
-*/
 
 console.log(
   `${country} is in ${continent}, and its ${population} million people speak ${language}`
-);
+  );
+  */
+
+/* 
+LECTURE: Taking Decisions: if / else Statements 
+1. If your country's population is greater that 33 million, log a string like this to the console: 'Portugal's population is above average'. Otherwise, log a string like 
+'Portugal's population is 22 million below average' (the 22 is the average of 33 minus the country's population) 
+2. After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original 
+
+if (population > 1300000000) {
+  console.log(`${country}'s population is above average.`);
+} else {
+  console.log(
+    `${country}'s population is ${33000000 - population} million below average`
+    );
+  }
+  */
 
 /*
 -------------------------------------------------------------
