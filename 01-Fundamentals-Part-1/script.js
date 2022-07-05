384a89efb69608803ec287efe5d433a251e1ed9b
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-05 20:25:43 +0300

    Taking Decisions: if / else Statements

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -123,6 +123,26 @@ lines");
 console.log(`String
 multiple
 lines`);
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving licence 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
 */
 
 /*
