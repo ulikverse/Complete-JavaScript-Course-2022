376d0e9e2d9be345d336074259d6a252cf3155b4
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-06 18:17:15 +0300

    Truthy and Falsy Values

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -143,7 +143,6 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
-*/
 
 // Type conversion
 const inputYear = "1991";
@@ -163,6 +162,29 @@ console.log("23" / "2");
 let n = "1" + 1; // "11"
 n = n - 1; // '11' - 1 = 10
 console.log(n);
+*/
+
+// 5 falsy values: 0, '', null, NaN, undefined
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job");
+}
+
+let height = 0;
+if (height) {
+  console.log("YAY! The height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
 
 /*
 ----------------------------------------------------------
