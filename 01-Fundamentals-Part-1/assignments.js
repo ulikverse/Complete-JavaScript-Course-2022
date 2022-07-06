fe50067028cfd611c92f5982b3b0f979aeec0c4d
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-06 17:52:09 +0300

    LECTURE: Type Conversion and Coercion

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -164,6 +164,22 @@ if (markBMI > johnBMI) {
 }
 
 /*
+LECTURE: Type Conversion and Coercion 
+1. Predict the result of these 5 operations without executing them: 
+'9' - '5'; 
+'19' - '13' + '17'; 
+'19' - '13' + 17; 
+'123' < 57; 
+5 + 6 + '4' + 9 - 4 - 2; 
+2. Execute the operations to check if you were right
+console.log("9" - "5"); // 4
+console.log("19" - "13" + "17"); // '617'
+console.log("19" - "13" + 17); // 23
+console.log("123" < 57); // ?
+console.log(5 + 6 + "4" + 9 - 4 - 2); // 1143
+*/
+
+/*
 -------------------------------------------------------------
 ----------------------------------------------------------
 ----------------------------------------------------------
