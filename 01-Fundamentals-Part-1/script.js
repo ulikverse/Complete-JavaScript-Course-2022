28123f24843fcb4075849a608b3e1f478b0bf21c
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-07 20:53:07 +0300

    Statements and Expressions

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -229,53 +229,65 @@ console.log(!hasDriversLicence);
     } else {
       console.log("Someone else should drive");
     }
+    
+    const day = "saturday";
+    
+    // switch (day) {
+      //   case "monday": // 'day' === 'monday'
+      //     console.log("Plan course structure");
+      //     console.log("Go to coding meetup");
+      //     break;
+      //   case "tuesday":
+      //     console.log("Prepare theory videos");
+      //     break;
+      //   case "wednesday":
+      //   case "thursday":
+      //     console.log("Write code examples");
+      //     break;
+      //   case "friday":
+      //     console.log("Record videos");
+      //     break;
+      //   case "saturday":
+      //   case "sunday":
+      //     console.log("Enjoy the weekend :D");
+      //     break;
+      //   default:
+      //     console.log("Not a valid day!");
+      // }
+      
+      if (day === "monday") {
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+      } else if (day === "tuesday") {
+        console.log("Prepare theory videos");
+      } else if (day === "wednesday" || day === "thursday") {
+        console.log("Write code examples");
+      } else if (day === "friday") {
+        console.log("Record videos");
+      } else if (day === "saturday" || day === "sunday") {
+        console.log("Enjoy the weekend :D");
+      } else {
+        console.log("Not a valid day!");
+      }
 */
+// Expression
+3 + 4;
+1991;
+true && false && !false;
 
-const day = "saturday";
-
-// switch (day) {
-//   case "monday": // 'day' === 'monday'
-//     console.log("Plan course structure");
-//     console.log("Go to coding meetup");
-//     break;
-//   case "tuesday":
-//     console.log("Prepare theory videos");
-//     break;
-//   case "wednesday":
-//   case "thursday":
-//     console.log("Write code examples");
-//     break;
-//   case "friday":
-//     console.log("Record videos");
-//     break;
-//   case "saturday":
-//   case "sunday":
-//     console.log("Enjoy the weekend :D");
-//     break;
-//   default:
-//     console.log("Not a valid day!");
-// }
-
-if (day === "monday") {
-  console.log("Plan course structure");
-  console.log("Go to coding meetup");
-} else if (day === "tuesday") {
-  console.log("Prepare theory videos");
-} else if (day === "wednesday" || day === "thursday") {
-  console.log("Write code examples");
-} else if (day === "friday") {
-  console.log("Record videos");
-} else if (day === "saturday" || day === "sunday") {
-  console.log("Enjoy the weekend :D");
-} else {
-  console.log("Not a valid day!");
+// Statement
+if (23 > 10) {
+  const str = "23 is bigger";
 }
 
+const me = "Jonas";
+console.log(`I'm ${2037 - 1991} years old ${me}`);
+
 /*
-----------------------------------------------------------
-----------------------------------------------------------
-----------------------------------------------------------
-let js = "amazing";
+      ----------------------------------------------------------
+      ----------------------------------------------------------
+      ----------------------------------------------------------
+      let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
