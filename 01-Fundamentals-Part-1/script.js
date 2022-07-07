8a0ea989258866449df6cd17db7699d7678edbd9
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-07 19:34:36 +0300

    Logical Operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -207,6 +207,28 @@ if (favorite === 23) {
 if (favorite !== 23) {
   console.log("Why not the 23?");
 }
+
+const hasDriversLicence = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicence && hasGoodVision);
+console.log(hasDriversLicence || hasGoodVision);
+console.log(!hasDriversLicence);
+
+// if (hasDriversLicence && hasGoodVision) {
+  //   console.log("Sarah is able to drive!");
+  // } else {
+    //   console.log("Someone else should drive");
+    // }
+    
+    const isTired = false; // C
+    console.log(hasDriversLicence && hasGoodVision && isTired);
+    
+    if (hasDriversLicence && hasGoodVision && !isTired) {
+      console.log("Sarah is able to drive!");
+    } else {
+      console.log("Someone else should drive");
+    }
 */
 
 /*
