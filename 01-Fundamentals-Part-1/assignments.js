bf22c1b1743ae117f6a09fee16045287ab4a777b
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-09 19:20:07 +0300

    fix

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -294,14 +294,15 @@ Hints:
 § Value X is between 50 and 300, if it's >= 50 && <= 300 😉 
  
 GOOD LUCK 😀
-*/
 
 let bill = 275;
 let tip = bill >= 50 && bill <= 300 ? (tip = bill * 0.15) : (tip = bill * 0.2);
 
 console.log(
   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
-);
+  );
+*/
+
 /*
 -------------------------------------------------------------
 ----------------------------------------------------------
