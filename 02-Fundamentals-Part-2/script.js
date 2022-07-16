74c9af8739cfdded4bffb7c5a6113fef8eaca9bb
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-16 17:20:01 +0300

    LECTURE: Function Declarations vs. Expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -69,6 +69,36 @@ console.log(age1, age2);
 */
 
 /*
+LECTURE: Function Declarations vs. Expressions 
+
+1. The world population is 7900 million people. Create a function declaration called 'percentageOfWorld1' which receives a 'population' value, and returns the percentage of the world population that the given population 
+represents. For example, China has 1441 million people, so it's about 18.2% of the world population 
+2. To calculate the percentage, divide the given 'population' value by 7900 and then multiply by 100 
+3. Call 'percentageOfWorld1' for 3 populations of countries of your choice, store the results into variables, and log them to the console 
+4. Create a function expression which does the exact same thing, called 'percentageOfWorld2', and also call it with 3 country populations (can be the same populations) 
+
+
+// function declaration
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+// function expression
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const popUkr = percentageOfWorld1(45);
+const popPol = percentageOfWorld1(38);
+const popChi = percentageOfWorld1(1441);
+console.log(popUkr, popPol, popChi);
+
+const popUkra = percentageOfWorld2(45);
+const popPola = percentageOfWorld2(38);
+const popChin = percentageOfWorld2(1441);
+console.log(popUkra, popPola, popChin);
+*/
+
+/*
 ------------------------------------------
 ------------------------------------------
 ------------------------------------------
