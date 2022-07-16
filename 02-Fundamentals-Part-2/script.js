4bfc4529add805ed008fdbd42d9c592862a9858f
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-16 18:44:07 +0300

    LECTURE: Functions Calling Other Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -162,7 +162,31 @@ function fruitProcessor(apples, oranges) {
 console.log(fruitProcessor(2, 3));
 */
 
--
+/*
+LECTURE: Functions Calling Other Functions 
+
+1. Create a function called 'describePopulation'. Use the function type you 
+like the most. This function takes in two arguments: 'country' and 
+'population', and returns a string like this: 'China has 1441 million people, 
+which is about 18.2% of the world.' 
+2. To calculate the percentage, 'describePopulation' call the 
+'percentageOfWorld1' you created earlier 
+3. Call 'describePopulation' with data for 3 countries of your choice
+
+// Function expression
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const describePopulation = function (country, population) {
+  const percentage = percentageOfWorld2(population);
+  
+  return `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+};
+console.log(describePopulation("Ukraine", 42));
+*/
+
+a;
 
 /*
 ------------------------------------------
