7da28dcc13bf71bf2c8990254dc5130cea266ff3
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-16 18:30:20 +0300

    Functions Calling Other Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -146,7 +146,23 @@ const popUkrai = percentageOfWorld3(45);
 const popPolan = percentageOfWorld3(38);
 const popChina = percentageOfWorld3(1441);
 console.log(popUkra, popPola, popChin);
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  
+  const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} pieces oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
 */
+
+-
 
 /*
 ------------------------------------------
