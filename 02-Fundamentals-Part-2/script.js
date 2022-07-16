43600cb85b90ea27a59ba22961de3a0d558eec28
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-16 16:10:41 +0300

    Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,4 +1,24 @@
 "use strict";
+
+function logger() {
+  console.log("My name is Jonas");
+}
+
+// calling / running / invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
 /* 
 let hasDriversLicence = false;
 const passTest = true;
