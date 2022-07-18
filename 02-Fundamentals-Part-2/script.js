ae519dd819984f357fe976c6e655b2893c6105fa
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-18 19:40:23 +0300

    Introduction to Array

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -258,4 +258,47 @@ const checkWinner = function (avgDolphins, avgKoalas) {
   };
   
   checkWinner(scoreDolhins, scoreKoalas);
+  
+  const friend1 = "Michael";
+  const friend2 = "Steven";
+  const friend3 = "Peter";
+  
+  const friends = ["Michael", "Steven", "Peter"];
+  console.log(friends);
+  
+  const y = new Array(1991, 1984, 2008, 2020);
+  
+  console.log(friends[0]);
+  console.log(friends[2]);
+  
+  console.log(friends.length);
+  console.log(friends[friends.length - 1]);
+  
+  friends[2] = "Jay";
+  console.log(friends);
+  // friends = ["Bob", "Alice"];
+  
+  const firstName = "Jonas";
+  const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
+  console.log(jonas);
+  console.log(jonas.length);
+  
+  // Exercise
+  const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+  };
+  const years = [1990, 1967, 2002, 2010, 2018];
+  
+  const age1 = calcAge(years[0]);
+  const age2 = calcAge(years[1]);
+  const age3 = calcAge(years[years.length - 1]);
+  console.log(age1, age2, age3);
+  
+  const ages = [
+    calcAge(years[0]),
+    calcAge(years[1]),
+    calcAge(years[years.length - 1]),
+  ];
+  console.log(ages);
+  
   */
