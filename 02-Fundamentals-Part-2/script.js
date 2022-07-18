5cedb5213831c714ef3e31e33050b4280decffce
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-18 19:55:35 +0300

    LECTURE: Introduction to Arrays

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -300,5 +300,25 @@ const checkWinner = function (avgDolphins, avgKoalas) {
     calcAge(years[years.length - 1]),
   ];
   console.log(ages);
-  
-  */
+*/
+
+/*
+LECTURE: Introduction to Arrays 
+
+1. Create an array containing 4 population values of 4 countries of your choice. You may use the values you have been using previously. Store this array into a 
+variable called 'populations' 
+2. Log to the console whether the array has 4 elements or not (true or false) 
+3. Create an array called 'percentages' containing the percentages of the world population for these 4 population values. Use the function 'percentageOfWorld1' that you created earlier to compute the 4 percentage values
+
+const populations = [42, 37, 1402, 329];
+console.log(populations.length === 4);
+
+const percentages = [
+  (populations[0] / 7900) * 100,
+  (populations[1] / 7900) * 100,
+  (populations[2] / 7900) * 100,
+  (populations[3] / 7900) * 100,
+];
+
+console.log(percentages);
+*/
