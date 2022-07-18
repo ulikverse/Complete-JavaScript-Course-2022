7230b6cc6f3ed5991ed73d5e2b163af37f87e1ea
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-18 20:14:43 +0300

    Basic Array Operations (Methods)

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -321,4 +321,36 @@ const percentages = [
 ];
 
 console.log(percentages);
+
+const friends = ["Michael", "Steven", "Peter"];
+
+// Add elements
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+// Remove elements
+friends.pop(""); // Last
+const popped = friends.pop("");
+console.log(popped);
+console.log(friends);
+
+friends.shift("");
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23));
+
+if (friends.includes("Steven")) {
+  console.log("You have a friend called Steven");
+}
+
 */
