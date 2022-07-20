742f5be15eea0e7a34b750fb011ff2e801782415
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-20 17:42:58 +0300

    Dot vs. Bracket Notation

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -468,3 +468,47 @@ const myCountry = {
 };
 
 */
+
+/*
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+
+// console.log(jonas."last" + nameKey);
+
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friens"
+  );
+  
+  if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+  } else {
+    console.log(
+      "Wrong request! Choose between firstName, lastName, age, job, and friens"
+      );
+    }
+    
+    jonas.location = "Portugal";
+    jonas["twitter"] = "@jonasschmedtmann";
+    console.log(jonas);
+    
+    // Challenge
+    // "Jonas has 3 frineds, and his best friend is called Michael!"
+    
+    console.log(
+      `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}!`
+      );
+      
+*/
