075d9b4de0d4daa13abffcb71ab633fbed39e6bc
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-20 17:47:00 +0300

    LECTURE: Dot vs. Bracket Notation

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -512,3 +512,32 @@ const interestedIn = prompt(
       );
       
 */
+
+/*
+LECTURE: Dot vs. Bracket Notation 
+
+1. Using the object from the previous assignment, log a string like this to the console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries 
+and a capital called Helsinki.' 
+2. Increase the country's population by two million using dot notation, and then decrease it by two million using brackets notation.
+
+const myCountry = {
+  country: "Ukraine",
+  capital: "Kyiv",
+  language: "ukrainian",
+  population: 42,
+  neighbours: [
+    "Poland",
+    "Slovakia",
+    "Hungary",
+    "Romania",
+    "Moldova",
+    "Orks",
+    "Semi-Orks",
+  ],
+};
+
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
+  );
+  
+  */
