70f2cf44a5b2eb5db1a7eac37caad9e7603a2fc4
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-25 22:27:22 +0300

    LECTURE: The while Loop

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -869,3 +869,23 @@ console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
   }
   
   */
+
+/*
+LECTURE: The while Loop 
+
+1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing', 
+but this time using a while loop (call the array 'percentages3') 
+2. Reflect on what solution you like better for this task: the for loop or the while 
+loop?
+  */
+
+const percentages3 = [];
+
+let i = 0;
+while (i < populations.length) {
+  const perc = percentageOfWorld1(populations[i]);
+  percentages3.push(perc);
+  i++;
+}
+
+console.log(percentages3);
