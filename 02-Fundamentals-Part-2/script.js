7568816c238d8cf8cef48a0e6adb33d35e472544
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-25 21:19:35 +0300

    Looping Backwards and Loops in Loops

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -796,4 +796,30 @@ console.log(percentages2);
   
   // RESULT IS THE SAME
   
+  
+  // Loop backwards
+  
+  const jonas = [
+    "Jonas",
+    "Schmedtmann",
+    2037 - 1991,
+    "teacher",
+    ["Michael", "Peter", "Steven"],
+    true,
+  ];
+  
+  for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+  }
+  
+  // Loop inside a loop
+  
+  for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`------- Starting exercise ${exercise}`);
+    
+    for (let rep = 1; rep < 6; rep++) {
+      console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♂️`);
+    }
+  }
+  
 */
