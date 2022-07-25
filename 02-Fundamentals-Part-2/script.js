25cb320032c6ffbf51475aa593476a449ed4d9fc
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-25 20:51:08 +0300

    LECTURE: Looping Arrays, Breaking and Continuing

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -760,3 +760,40 @@ for (let i = 0; i < jonas.length; i++) {
 }
 
 */
+
+/*
+LECTURE: Looping Arrays, Breaking and Continuing 
+
+1. Let's bring back the 'populations' array from a previous assignment 
+2. Use a for loop to compute an array called 'percentages2' containing the percentages of the world population for the 4 population values. Use the function 'percentageOfWorld1' that you created earlier 
+3. Confirm that 'percentages2' contains exactly the same values as the 'percentages' array that we created manually in the previous assignment, and reflect on how much better this solution is
+
+
+// new solution
+
+const populations = [42, 37, 1402, 329];
+const percentages2 = [];
+
+for (let i = 0; i < populations.length; i++) {
+  percentages2.push((populations[i] / 7900) * 100);
+}
+
+console.log(percentages2);
+
+// old solution
+
+// const populations = [42, 37, 1402, 329];
+// console.log(populations.length === 4);
+
+// const percentages = [
+  //   (populations[0] / 7900) * 100,
+  //   (populations[1] / 7900) * 100,
+  //   (populations[2] / 7900) * 100,
+  //   (populations[3] / 7900) * 100,
+  // ];
+  
+  // console.log(percentages);
+  
+  // RESULT IS THE SAME
+  
+*/
