ce70f688eedef520972b17d898a16911fe2f272a
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-26 09:16:40 +0300

    Setting up Prettier and VS Code

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+if (x === 23) console.log(23);
+
+const caclAge = birthYear => 2037 - birthYear;
+
+console.log();
