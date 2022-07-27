d55ae91601ddd6b44951d24367436a25ec806834
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-27 08:57:05 +0300

    Installing Node.js and Setting Up a Dev Environment

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,9 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
-if (x === 23) console.log(23);
+const x = '23';
 
 const caclAge = birthYear => 2037 - birthYear;
 
-console.log();
+console.log(caclAge(1991));
