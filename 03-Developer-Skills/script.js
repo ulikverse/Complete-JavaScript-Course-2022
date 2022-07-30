12abc89915a2aba419a2a3a789b84bd09aaa8fca
Yulii Shtel <ulik1shtel@gmail.com>
2022-07-30 15:59:21 +0300

    Debugging with the Console and Breakpoints

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -73,4 +73,51 @@ const calcTempAmplitudeNew = function (t1, t2) {
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
 
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // C) FIX THE BUG
+    // value: Number(prompt('Degrees celsius:')),
+    value: 10,
+  };
+  
+  // B) FIND A BUG
+  // console.log(measurement);
+  console.table(measurement);
+  
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY THE BUG
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  
+  let max = 0;
+  let min = 0;
+  
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY THE BUG
+console.log(amplitudeBug);
+
 */
