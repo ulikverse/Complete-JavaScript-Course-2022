09d0ab56e27a5a3e8ee87f998732c20d693f0954
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-01 19:53:45 +0300

    Handling Click Events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,12 +1,25 @@
 'use strict';
 
 /*
- */
 
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = ' 🎉 Correct Number';
+
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    console.log(
+      (document.querySelector('.message').textContent = ' ⛔️ No Number!')
+    );
+  }
+});
