b90c3bf60e5f2521cf2802ec1b9ebdbf06f7fdf8
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-02 19:10:20 +0300

    Implementing the Game Logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -13,6 +13,11 @@ console.log(document.querySelector('.guess').value);
 
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -21,5 +26,25 @@ document.querySelector('.check').addEventListener('click', function () {
     console.log(
       (document.querySelector('.message').textContent = ' ⛔️ No Number!')
     );
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = ' 🎉 Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You loose the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You loose the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
