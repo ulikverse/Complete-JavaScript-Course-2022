22fc829c4c788730eb074c4f20ae723959b79c20
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-02 20:11:15 +0300

    JavaScript in the Browser: DOM and Events Coding Challenge #1

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -13,10 +13,8 @@ console.log(document.querySelector('.guess').value);
 
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -31,9 +29,9 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = ' 🎉 Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
 
     // When guess is too high
@@ -59,3 +57,33 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  // displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+/*
+JavaScript in the Browser: DOM and Events  
+ 
+Coding Challenge #1 
+
+Implement a game rest functionality, so that the player can make a new guess! 
+
+Your tasks: 
+1. Select the element with the 'again' class and attach a click event handler 
+2. In the handler function, restore initial values of the 'score' and 'secretNumber' variables 
+3. Restore the initial conditions of the message, number, score and guess input fields 
+4. Also restore the original background color (#222) and number width (15rem) 
+ 
+GOOD LUCK 😀
+*/
