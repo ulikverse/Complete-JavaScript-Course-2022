a02bce4fc1efba9b0f126441d1fcdab3ba401a29
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-03 18:36:46 +0300

    fix

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -4,7 +4,6 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-// console.log(btnsOpenModal);
 
 const openModal = function () {
   modal.classList.remove('hidden');
