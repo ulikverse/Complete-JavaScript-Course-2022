653bc3928a223a46f32f298134fe1cb393bc6ca7
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-06 21:09:20 +0300

    Switching the Active Player

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selection elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -16,7 +18,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -32,8 +36,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to the current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
