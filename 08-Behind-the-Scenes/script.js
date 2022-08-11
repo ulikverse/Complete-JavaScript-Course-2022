ab2ec03234b9b7342787e4bf4b7cd735b9985fbc
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-11 19:17:10 +0300

    egular Functions vs. Arrow Functions

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -80,7 +80,6 @@ console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
 
-*/
 
 // console.log(this);
 
@@ -116,3 +115,53 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 f();
+
+
+var firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+    
+    // Solution 1
+    // const self = this; // sefl or that
+    // const isMillenial = function () {
+      //   console.log(self);
+      //   console.log(self.year >= 1980 && self.year <= 1996);
+      //   // console.log(this.year >= 1980 && this.year <= 1996);
+      // };
+      
+      // Solution 2
+      const isMillenial = () => {
+        console.log(this);
+        console.log(this.year >= 1980 && this.year <= 1996);
+      };
+      isMillenial();
+    },
+    
+    greet: () => {
+      console.log(this);
+      console.log(`Hey ${this.firstName}`);
+    },
+  };
+  jonas.greet();
+  jonas.calcAge();
+  
+  // arguments keyword
+  const addExpr = function (a, b) {
+    console.log(arguments);
+    return a + b;
+  };
+  addExpr(2, 5);
+  addExpr(2, 5, 8, 12);
+  
+  var addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b;
+  };
+  addArrow(2, 5, 8);
+  
+  */
