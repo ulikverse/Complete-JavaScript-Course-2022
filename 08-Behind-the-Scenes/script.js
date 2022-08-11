f01ea7a0ed419f4b9f56db19d31fbd65095ba703
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-11 19:32:00 +0300

    Primitives vs. Objects (Primitive vs. Reference Types)

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -164,4 +164,21 @@ const jonas = {
   };
   addArrow(2, 5, 8);
   
+  
+  let age = 30;
+  let oldAge = 30;
+  age = 31;
+  console.log(age);
+  console.log(oldAge);
+  
+  const me = {
+    name: 'Jonas',
+    age: 30,
+  };
+  
+  const friend = me;
+  friend.age = 27;
+  console.log('Friend:', friend);
+  console.log('Me:', me);
+  
   */
