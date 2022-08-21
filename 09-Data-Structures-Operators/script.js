8e5223a9c77a0be12c000d8a9de7a193d00f4b5e
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-21 13:07:06 +0300

    fix

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -48,11 +48,13 @@ const restaurant = {
 };
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+
-  _Arrival;bru0943384722;fao93766109;11:45+
-  _Delayed_Arrival;hel7439299980;fao93766109;12:05+
-  _Departure;fao93766109;lis2323639855;12:30';
+
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+
+//   _Arrival;bru0943384722;fao93766109;11:45+
+//   _Delayed_Arrival;hel7439299980;fao93766109;12:05+
+//   _Departure;fao93766109;lis2323639855;12:30';
+
 /*
 // Split and join
 console.log('a+very+nice+string'.split('+'));
