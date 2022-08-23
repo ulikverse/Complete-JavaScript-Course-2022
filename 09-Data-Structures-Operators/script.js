470978fc09af144d9f093994ab9d5e5dc14f098f
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-23 22:47:02 +0300

    Logical Assignment Operators

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -49,6 +49,37 @@ const restaurant = {
   },
 };
 
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assigment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// NULISH assigment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assigment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS';
+// rest2.owner = rest2.owner && '<ANONYMOUS';
+rest1.owner &&= '<ANONYMOUS';
+rest2.owner &&= '<ANONYMOUS';
+
+console.log(rest1);
+console.log(rest2);
+
 /*
 restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
