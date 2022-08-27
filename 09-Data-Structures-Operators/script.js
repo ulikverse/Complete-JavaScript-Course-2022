f3bf4750856de1914591021a9c5a0862dc0322b2
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-27 22:43:08 +0300

    Maps: Fundamentals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -49,6 +49,39 @@ const restaurant = {
   },
 };
 
+/*
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed ):');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 23;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+const arr = [1, 2];
+console.log(rest.has('categories'));
+rest.delete(2);
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+*/
+
+/*
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
@@ -83,6 +116,7 @@ console.log(
 );
 
 console.log(new Set('jonasschmedtmann').size);
+*/
 
 /*
 Coding Challenge #2 
