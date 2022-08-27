bf7698b9f832466a8e66fbb5e7a018280ae4db35
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-28 00:37:51 +0300

    Coding Challenge #3

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -50,6 +50,79 @@ const restaurant = {
 };
 
 /*
+Coding Challenge #3 
+
+Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time). 
+
+Your tasks: 
+1. Create an array 'events' of the different game events that happened (no duplicates) 
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log. 
+3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes) 
+4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this: 
+
+[FIRST HALF] 17: ⚽ GOAL 
+ 
+GOOD LUCK 😀
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+
+const deteleCard = gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+// const x = 90;
+// const y = gameEvents.size;
+// const z = x / y;
+
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+
+// [FIRST HALF] 17: ⚽ GOAL
+
+// for (const [key, value] of gameEvents) {
+//   if (value < 45) {
+//     console.log(`[FIRST HALF]: {key}: ${value}`);
+//   } else {
+//     console.log(`[SECOND HALF]: {key}: ${value}`);
+//   }
+// }
+
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+/
+
+/*
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
   [1, 'C '],
