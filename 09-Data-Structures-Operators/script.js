11ec04fbab9f4ec07544a083a3927966bf3fe1a7
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-28 21:41:21 +0300

    Coding Challenge #4

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -81,6 +81,26 @@ GOOD LUCK 😀
 
 */
 
+/*
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
+*/
+
 // const flights =
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25
 //   +_Arrival;bru0943384722;fao93766109;11:45
