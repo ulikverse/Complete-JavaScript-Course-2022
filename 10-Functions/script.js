22453f8924b1f00172eabb80d7d8cecfeb91e9f9
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-29 22:50:23 +0300

    Functions Accepting Callback Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -72,3 +72,40 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 */
+
+/*
+
+//// Functions Accepting Callback Functions
+//// Functions Accepting Callback Functions
+//// Functions Accepting Callback Functions
+
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+transformer('JavaScript is the best!', upperFirstWord);
+
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callback all the time
+
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+*/
