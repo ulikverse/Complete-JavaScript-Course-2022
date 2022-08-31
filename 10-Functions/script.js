46c2c54e690a448d91f8fa0b575d8ade9cd15ccc
Yulii Shtel <ulik1shtel@gmail.com>
2022-08-31 18:14:09 +0300

    Coding Challenge #1

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -246,6 +246,7 @@ console.log(addVAT2(23));
 Coding Challenge #1 
 
 Let's build a simple poll app! 
+
 A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter 'poll' object below. 
 
 Your tasks: 
@@ -280,14 +281,52 @@ GOOD LUCK 😀
 
 */
 
+/*
+//// Coding Challenge #1
+//// Coding Challenge #1
+//// Coding Challenge #1
+//// Coding Challenge #1
+
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
-  registerNewAnswer: function () {},
-  displayResults: function () {},
+  registerNewAnswer: function () {
+    // Get the answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n('Write option number')`
+      )
+    );
+    console.log(answer);
+
+    // Update (Register) the answer
+    typeof answer === 'number' &&
+      answer < this.options.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      // Poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
 };
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6] });
+*/
 
 /*
 //// Immediately Invoked Function Expressions (IIFE)
