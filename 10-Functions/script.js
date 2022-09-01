1578a6db479c7b7eaf459db1bc714560d4685e82
Yulii Shtel <ulik1shtel@gmail.com>
2022-09-01 23:35:47 +0300

    Coding Challenge #2

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -501,3 +501,35 @@ const boardPasengers = function (n, wait) {
 // const perGroup = 1000;
 boardPasengers(180, 3);
 */
+
+////
+
+/*
+Coding Challenge #2 
+
+This is more of a thinking challenge than a coding challenge 🤓 
+
+Your tasks: 
+
+1. Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time 
+the body element is clicked. Do not select the h1 element again! 
+
+2. And now explain to yourself (or someone around you) why this worked! Take all the time you need. Think about when exactly the callback function is executed, and what that means for the variables involved in this example. 
+
+GOOD LUCK 😀 
+
+*/
+
+/*
+
+//// Coding Challenge #2
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+*/
