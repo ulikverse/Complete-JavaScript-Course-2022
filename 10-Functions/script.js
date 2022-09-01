de4cf00cebbc7a31ce787c140860ee501c1cdcf4
Yulii Shtel <ulik1shtel@gmail.com>
2022-09-01 23:23:43 +0300

    More Closure Examples

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -453,3 +453,51 @@ booker();
 console.dir(booker);
 
 */
+
+// Example 1
+
+/*
+let f = 0;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(g);
+
+// Re-assigning the f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPasengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers `);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// setTimeout(function () {
+//   console.log('TIMER');
+// }, 1000);
+
+// const perGroup = 1000;
+boardPasengers(180, 3);
+*/
