a102c985a902471d4f5e50dfe20e843931b4af88
Yulii Shtel <ulik1shtel@gmail.com>
2022-09-06 21:15:07 +0300

    fix

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -134,6 +134,7 @@ console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
 */
 
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // for (const movement of movements) {
@@ -158,3 +159,5 @@ movements.forEach(function (mov, i, arr) {
 // 1: function(450) {}
 // 2: function(400) {}
 // ...
+
+*/
