836b07b8d4acc944fd83dab240f5bcb4ba6aec68
Yulii Shtel <ulik1shtel@gmail.com>
2022-09-20 19:16:57 +0300

    Computing Usernames

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -86,6 +86,19 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
+
 /*
 Working With Arrays  
  
