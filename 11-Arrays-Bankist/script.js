9daa1e31697e32fe6839b6235b0fb4a197e577cf
Yulii Shtel <ulik1shtel@gmail.com>
2022-09-26 19:45:18 +0300

    Coding Challenge #3

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -451,6 +451,40 @@ const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
 */
 
+/*
+Coding Challenge #3 
+
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time as an arrow function, and using chaining! 
+
+Test data: 
+§ Data 1: [5, 2, 4, 1, 15, 8, 3] 
+§ Data 2: [16, 6, 10, 5, 6, 1, 4] 
+ 
+GOOD LUCK 😀 
+
+const calcAverageHumanAge2 = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+  const average = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+    );
+    return average;
+  };
+  
+  const calcAverageHumanAge = ages =>
+  ages
+  .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+  .filter(age => age >= 18)
+  .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+  
+  const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+  const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+  console.log(avg1, avg2);
+  
+  ////
+  */
+
 const eurToUsd = 1.1;
 console.log(movements);
 
@@ -463,4 +497,4 @@ const totalDepositsUSD = movements
   })
   // .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
-console.log(totalDepositsUSD);
+// console.log(totalDepositsUSD);
