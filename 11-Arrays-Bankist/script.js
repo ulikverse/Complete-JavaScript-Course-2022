3c09355bad0aee7505dd5ee81125f781d7d2cd95
Yulii Shtel <ulik1shtel@gmail.com>
2022-09-28 16:57:37 +0300

    flat and flatMap

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -637,4 +637,22 @@ console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
 
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// flat
+const overalBalance = accounts
+.map(acc => acc.movements)
+.flat()
+.reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// flataMap
+const overalBalance2 = accounts
+.flatMap(acc => acc.movements)
+.reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
 */
