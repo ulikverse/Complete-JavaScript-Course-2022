6bfdf2947bfb02ab5227b3f8757c00772fb72ba7
Yulii Shtel <ulik1shtel@gmail.com>
2022-10-17 18:11:39 +0300

    Implementing Smooth Scrolling

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -36,6 +36,7 @@ document.addEventListener('keydown', function (e) {
 /////////////////////////////////////////
 /////////////////////////////////////////
 
+/*
 // Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -118,3 +119,36 @@ logo.classList.contains('c');
 
 // Don't use
 logo.className = 'jonas';
+*/
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1cords = section1.getBoundingClientRect();
+  console.log(s1cords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1cords.left + window.pageXOffset,
+  //   s1cords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1cords.left + window.pageXOffset,
+  //   top: s1cords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
