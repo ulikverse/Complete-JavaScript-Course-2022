1830f1e537ab43e15996d2a8fe53117d1315bf4e
Yulii Shtel <ulik1shtel@gmail.com>
2022-10-28 16:18:53 +0300

    ES6 Classes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -120,3 +120,39 @@ bmw.accelerate();
 bmw.brake();
 bmw.accelerate();
 */
+
+// class expression
+// const PersonCl = class {};
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthtYear) {
+    this.firstName = firstName;
+    this.birthtYear = birthtYear;
+  }
+
+  // Methods will be added to .prototype
+  calcAge() {
+    console.log(2037 - this.birthtYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
