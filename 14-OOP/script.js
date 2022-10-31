dee5fc823ddc7c4a1d59f73ce12254902e1319e8
Yulii Shtel <ulik1shtel@gmail.com>
2022-10-31 22:19:21 +0200

    Encapsulation: Private Class Fields and Methods

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -505,52 +505,76 @@ jay.calcAge();
 //////////////////////////
 //// Another Class Example
 
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also a static version)
 class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
     // protected property
-    this._pin = pin;
-    this._movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    // this.#movements = [];
+    // this.locale = navigator.lang uage;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
+  // 3) Public methods
 
   // Public interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  _approveLoan() {
-    return true;
-  }
-
   requestLoan(val) {
+    // if (this.#approveLoan(val)) {
     if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
   }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  _approveLoan() {
+    return true;
+  }
 }
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
 
-// acc1._movements.push(250);
-// acc1._movements.push(-140);
+// acc1.#movements.push(250);
+// acc1.#movements.push(-140);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1._approveLoan(1000);
+// acc1._approveLoan(1000);
 console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+
+Account.helper();
