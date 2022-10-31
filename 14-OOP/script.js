ec1720a1d89ec2959a08ffe66069f4aa72a5e3d6
Yulii Shtel <ulik1shtel@gmail.com>
2022-10-31 21:45:43 +0200

    Encapsulation: Protected Properties and Methods

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -509,28 +509,33 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    // protected property
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
   // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan() {
+  _approveLoan() {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -539,11 +544,13 @@ class Account {
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+acc1._approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
+console.log(acc1.pin);
