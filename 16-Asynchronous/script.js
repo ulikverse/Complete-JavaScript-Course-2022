e35de2a2874047122c716a82e5e169a62d8edb7e
Yulii Shtel <ulik1shtel@gmail.com>
2022-11-08 15:48:30 +0200

    Chaining Promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -130,9 +130,20 @@ const request = fetch('https://restcountries.com/v2/name/portugal');
 console.log(request);
 
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('portugal');
