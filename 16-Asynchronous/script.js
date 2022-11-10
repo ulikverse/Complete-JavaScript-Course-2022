9fe98d7ccaf3e611563088feb2b8630eb5569738
Yulii Shtel <ulik1shtel@gmail.com>
2022-11-10 18:20:59 +0200

    Building a Simple Promise

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -302,3 +302,54 @@ Promise.resolve('Resovled promise 2').then(res => {
 console.log('Test end'); // 2
 
 */
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening 🔮');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 seconds passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 seconds passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 seconds passed'));
+
+// setTimeout(() => {
+//   '1 second passed';
+//   setTimeout(() => {
+//     '2 seconds passed';
+//     setTimeout(() => {
+//       '3 secons passed';
+//       setTimeout(() => {
+//         '4 seconds passed';
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.error(x));
