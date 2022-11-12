2507fa82a8252a93fe77bfcb2d3933c395697905
Yulii Shtel <ulik1shtel@gmail.com>
2022-11-12 17:09:51 +0200

    CommonJS Modules

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -43,8 +43,6 @@ console.log(lastPost);
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
 
-*/
-
 const ShoppingCart2 = (function () {
   const cart = [];
   const shippingCost = 10;
@@ -74,3 +72,16 @@ ShoppingCart2.addToCart('apples', 4);
 ShoppingCart2.addToCart('pizza', 2);
 console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
+
+
+// Export
+export.addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(
+    `${quantity} ${product} was added to cart (shipping cost is ${shippingCost})`
+  );
+};
+
+// Import
+const { addToCart } = require('./shoppingCart.js')
+*/
