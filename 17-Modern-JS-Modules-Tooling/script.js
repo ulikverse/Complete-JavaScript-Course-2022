207486dc62f13b72f7b37c35af832187f75da46e
Yulii Shtel <ulik1shtel@gmail.com>
2022-11-13 23:20:29 +0200

    Configuring Babel and Polyfilling

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -111,3 +111,23 @@ console.log(stateDeepClone);
 if (module.hot) {
   module.hot.accept();
 }
+
+class Person {
+  #greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+
+const jonas = new Person('Jonas');
+
+console.log('Jonas' ?? null);
+
+// console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+
+// Polifilling async functions
+import 'regenerator-runtime/runtime';
